50229d266235b0b5205194349486f447d44d907b
Dirty Swift <dirtyswift@gmail.com>
2026-04-20 20:29:08 +0200

    feat: shell extension MV3 avec content scripts Spotify et Deezer

---
+++ src/content-deezer.js
@@ -0,0 +1,4 @@
+// Content script Deezer — dirty-tracklist-grabber
+(() => {
+  console.log("[dirty-tracklist-grabber] Deezer content script chargé");
+})();
---
+++ src/content-spotify.js
@@ -0,0 +1,4 @@
+// Content script Spotify — dirty-tracklist-grabber
+(() => {
+  console.log("[dirty-tracklist-grabber] Spotify content script chargé");
+})();
